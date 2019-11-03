bd898cbd74256039483458828202f8d0ab1b573e
Cameron <cameron.turner1998@outlook.com>
2019-11-04 00:17:45 +0800

    Added check to see if map ret value is null.

--- syscount2.py
+++ syscount2.py
@@ -65,16 +65,16 @@ struct data_t{
     u32 sys; // syscall
 };
 
-/*struct data_exit {
+struct data_exit {
     u64 pid_tgid; // process id
     u32 pid32; //pid after the 32 bit shift
     u32 sys; // syscall
     u32 ret; //return arguments
-};*/
+};
 
 //BPF_HASH(count, u32, u32);
 BPF_HASH(data, u64, struct data_t, 500000);
-//BPF_HASH(data_exit_hash, u64, struct data_exit, 500000);
+BPF_HASH(data_exit_hash, u64, struct data_exit, 500000);
 
 
 //BPF_PERF_OUTPUT(events);
@@ -97,24 +97,30 @@ TRACEPOINT_PROBE(raw_syscalls, sys_enter){
     struct data_t *val, zero= {};
     u64 t = bpf_ktime_get_ns();
     val = data.lookup_or_init(&t, &zero);
-    val->pid_tgid = bpf_get_current_pid_tgid();
-    //val->pid32 = val->pid_tgid >> 32;
-    //val->sys = args->id;
+    
+    if(val){
+        val->pid_tgid = bpf_get_current_pid_tgid();
+        val->pid32 = val->pid_tgid >> 32;
+        val->sys = args->id;
+    }
+    
 
     return 0;
 }
 
-/*TRACEPOINT_PROBE(raw_syscalls, sys_exit){
+TRACEPOINT_PROBE(raw_syscalls, sys_exit){
     struct data_exit *val, zero={};
     u64 t = bpf_ktime_get_ns();
     val = data_exit_hash.lookup_or_init(&t, &zero);
-    val->pid_tgid = bpf_get_current_pid_tgid();
-    val->pid32 = val->pid_tgid >> 32;
-    val->sys = args->id;
-    val->ret = args->ret;
+    if(val){
+        val->pid_tgid = bpf_get_current_pid_tgid();
+        val->pid32 = val->pid_tgid >> 32;
+        val->sys = args->id;
+        val->ret = args->ret;
+    }
 
     return 0;
-}*/
+}
 """
 
 if args.pid:
@@ -156,13 +162,13 @@ def print_event_hash():
         if k.value == 0xFFFFFFFF:
             continue    # happens occasionally, we don't need it
         printb((b"%-20d %22s %8d  %20s") % (k.value, comm_for_pid(v.pid32), v.pid_tgid, syscall_name(v.sys)))
-
-    """data = bpf["data_exit_hash"]
+    print("STARTING EXIT PRINT")
+    data = bpf["data_exit_hash"]
     for k, v in sorted(data.items(), key=lambda kv: -kv[0].value, reverse = True):
         if k.value == 0xFFFFFFFF:
             continue    # happens occasionally, we don't need it
         printb((b"%-20d %22s %8d  %20s %15d") % (k.value, comm_for_pid(v.pid32), v.pid_tgid, syscall_name(v.sys), v.ret))
-"""
+
 """b["events"].open_perf_buffer(print_event_perf)
 while 1:
     try:
